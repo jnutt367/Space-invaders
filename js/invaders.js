f7ea7a08e7bdcf063f91182af33fdeb672d85b82
Jason Nutt <jnutt367@gmail.com>
2021-01-20 15:48:20 -0500

    Add files via upload
    Making current with the version in test

--- js/invaders.js
+++ js/invaders.js
@@ -11,9 +11,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // define the alien invaders
     const alienInvaders = [
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-        15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-        30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+        2, 3, 4, 5, 6, 7, 8,
+        16, 17, 18, 19, 20, 21, 22, 23, 24,
+        30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40
     ]
 
     //draw alien invaders
