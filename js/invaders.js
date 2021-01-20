430f9911c0f032249304ae50f20ee15d4b025c78
Jason Nutt <jnutt367@gmail.com>
2021-01-20 15:37:39 -0500

    Add files via upload
    Made a smaller version. To test easier

--- js/invaders.js
+++ js/invaders.js
@@ -19,22 +19,10 @@ document.addEventListener('DOMContentLoaded', () => {
     //draw alien invaders
     alienInvaders.forEach(invader => squares[currentInvaderIndex + invader].classList.add('invader'))
 
-
-    // draw shooter
-    squares[currentShooterIndex].classList.add('shooter')
-
-    // move the shooter
-    function moveShooter(e) {
-        squares[currentShooterIndex].classList.remove('shooter')
-        switch (e.keyCode) {
-            case 37:
-                if (currentShooterIndex % width !== 0) currentShooterIndex -= 1
-                break
-            case 39:
-                if (currentShooterIndex % width < width - 1) currentShooterIndex += 1
-                break
-        }
-        squares[currentShooterIndex].classList.add('shooter')
-    }
-    document.addEventListener('keydown', moveShooter)
 })
+
+
+// draw shooter
+
+
+// move the shooter
