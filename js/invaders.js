2d0e59933ec26f3fd3d021050658ce33ec30451a
Jason Nutt <jnutt367@gmail.com>
2021-01-20 18:35:10 -0500

    Added the Aliens movement logic
    Moving through the logic one function at a time...this is function moveInvaders( ) {...}

--- js/invaders.js
+++ js/invaders.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // define the alien invaders
     const alienInvaders = [
-        2, 3, 4, 5, 6, 7, 8,
+        2, 3, 4, 5, 6, 7, 8, 9,
         16, 17, 18, 19, 20, 21, 22, 23, 24,
         30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40
     ]
@@ -19,10 +19,64 @@ document.addEventListener('DOMContentLoaded', () => {
     //draw alien invaders
     alienInvaders.forEach(invader => squares[currentInvaderIndex + invader].classList.add('invader'))
 
+
+    // draw shooter
+    squares[currentShooterIndex].classList.add('shooter')
+
+    // move the shooter
+    function moveShooter(e) {
+        squares[currentShooterIndex].classList.remove('shooter')
+        switch (e.keyCode) {
+            case 37:
+                if (currentShooterIndex % width !== 0) currentShooterIndex -= 1
+                break
+            case 39:
+                if (currentShooterIndex % width < width - 1) currentShooterIndex += 1
+                break
+        }
+        squares[currentShooterIndex].classList.add('shooter')
+    }
+    document.addEventListener('keydown', moveShooter)
+
+    // move alien invaders 
+    function moveInvaders() {
+        const leftEdge = alienInvaders[0] % width === 0
+        const rightEdge = alienInvaders[alienInvaders.length - 1] % width === -1
+
+        if ((leftEdge && direction === -1) || (rightEdge && direction === 1)) {
+            direction = width
+        } else if (direction === width) {
+            if (leftEdge) direction = 1
+            else direction = -1
+        }
+        for (let i = 0; i <= alienInvaders.length - 1; i++) {
+            squares[alienInvaders[i]].classList.remove('invader')
+        }
+        for (let i = 0; i <= alienInvaders.length - 1; i++) {
+            alienInvaders[i] += direction
+        }
+        for (let i = 0; i <= alienInvaders.length - 1; i++) {
+            squares[alienInvaders[i]].classList.add('invader')
+        }
+        // Decide Game Over
+        if (squares[currentShooterIndex].classList.contains('invader', 'shooter')) {
+            resultDisplay.textContent = 'You got hit...Game is Over....You Are DEAD!!'
+            squares[currentShooterIndex].classList.add('boom')
+            clearInterval(invaderId)
+        }
+
+        for (let i = 0; i <= alienInvaders.length - 1; i++) {
+            if (alienInvaders[i] > (squares.length - (width - 1))) {
+                resultDisplay.textContent = 'You Let the Aliens Reach Earth!'
+                clearInterval(invaderId)
+            }
+        }
+    }
+    invaderId = setInterval(moveInvaders, 500)
+
+
+
+
+
+
 })
-
-
-// draw shooter
-
-
-// move the shooter
